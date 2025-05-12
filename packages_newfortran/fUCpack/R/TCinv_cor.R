28b1fa715533d574631474f8ab6043f050d2f743
tobiashartl <tobias.hartl32@gmail.com>
2025-05-12 15:11:13 +0200

    update packages

--- packages_newfortran/fUCpack/R/TCinv_cor.R
+++
@@ -1,7 +0,0 @@
-#' @export
-#' @useDynLib fUCpack TC_inverse_corr
-TCinv_cor <- function(Sd, B, sigma_eta, sigma_eps, cov_etaeps,
-                  y, n, s, b) .Call(TC_inverse_corr, S0t=Sd, S0c=B, sigma_eta = sigma_eta,
-                                    sigma_eps = sigma_eps, cov_etaeps = cov_etaeps,
-                                    y=y, n=as.integer(n), ma=s, ma_c=b)
-
